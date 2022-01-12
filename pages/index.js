4c49cf2b70a1a665e0923ee76e8eadde090fe4d7
ajoy <ajoykarmakar459@gmail.com>
2022-01-13 00:38:35 +0530

    All folders.

--- pages/index.js
+++ pages/index.js
@@ -11,6 +11,9 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
+         Hi
+        </h1>
+        <h1 className={styles.title}>
           Welcome to <a href="https://nextjs.org">Next.js</a> on Docker!
         </h1>
 
