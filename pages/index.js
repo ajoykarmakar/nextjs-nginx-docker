20edf00633fb1e1d2fee137348f2d823c47bdfa9
ajoy <ajoykarmakar459@gmail.com>
2022-01-13 16:43:04 +0530

    docker nextjs dev implemented.

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-         Hi
+         Hi there
         </h1>
         <h1 className={styles.title}>
           Welcome to <a href="https://nextjs.org">Next.js</a> on Docker!
